6bb868272e34526a8123726ad09c712653d787f3
VnkHO <ho.vincent.nk@gmail.com>
2020-02-11 15:51:47 +0100

    useState with form

--- src/App.js
+++ src/App.js
@@ -8,18 +8,30 @@ const App = () => {
   // const [value, setValue] = useState(initialState);
   const [name, setName] = useState('');
 
-
-
   return (
     <div className="main-wrapper">
       <h1>React Hooks Starter</h1>
       <div>
-        <h3>{name}</h3>
-        <input type="text" onChange={(event) => setName(event.target.value)} value={name} />
+        <form onSubmit={(event) => {
+          event.preventDefault();
+          formSubmit(name, setName);
+        }}>
+          <input
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+            value={name}
+          />
+          <button type="submit">Submit</button>
+        </form>
         <Toggle />
       </div>
     </div>
   );
 };
+
+const formSubmit = (value, setValue) => {
+  console.log(`email sent to ${value} !`)
+  setValue('');
+}
 
 export default App;
